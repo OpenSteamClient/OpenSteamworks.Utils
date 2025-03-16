6fbf7d7425c885c4bfab23b93dba9c607f736574
Onni Kukkonen <onni.kukkonen77@gmail.com>
2025-03-16 16:46:45 +0200

    fix: EndianAwareBinaryReader correctly throws EndOfStreamException

--- EndianAwareBinaryReader.cs
+++ EndianAwareBinaryReader.cs
@@ -39,6 +39,20 @@ public class EndianAwareBinaryReader : BinaryReader
         _endianness = endianness;
     }
 
+    /// <summary>
+    /// Reads at least a minimum number of bytes from the current stream and advances the position within the stream by the number of bytes read.
+    /// </summary>
+    /// <param name="minimumBytes">The minimum number of bytes to read into the buffer.</param>
+    /// <exception cref="ArgumentOutOfRangeException">minimumBytes is negative, or is greater than the length of buffer.</exception>
+    /// <exception cref="EndOfStreamException">The end of the stream is reached before reading minimumBytes bytes of data.</exception>
+    /// <returns>The bytes requested.</returns>
+    public byte[] ReadAtLeast(int minimumBytes)
+    {
+        byte[] arr = new byte[minimumBytes];
+        BaseStream.ReadAtLeast(arr, minimumBytes);
+        return arr;
+    }
+
     public override short ReadInt16() => ReadInt16(_endianness);
 
     public override int ReadInt32() => ReadInt32(_endianness);
@@ -52,26 +66,26 @@ public class EndianAwareBinaryReader : BinaryReader
     public override ulong ReadUInt64() => ReadUInt64(_endianness);
 
     public short ReadInt16(Endianness endianness) => endianness == Endianness.Little
-        ? BinaryPrimitives.ReadInt16LittleEndian(ReadBytes(sizeof(short)))
-        : BinaryPrimitives.ReadInt16BigEndian(ReadBytes(sizeof(short)));
+        ? BinaryPrimitives.ReadInt16LittleEndian(ReadAtLeast(sizeof(short)))
+        : BinaryPrimitives.ReadInt16BigEndian(ReadAtLeast(sizeof(short)));
 
     public int ReadInt32(Endianness endianness) => endianness == Endianness.Little
-        ? BinaryPrimitives.ReadInt32LittleEndian(ReadBytes(sizeof(int)))
-        : BinaryPrimitives.ReadInt32BigEndian(ReadBytes(sizeof(int)));
+        ? BinaryPrimitives.ReadInt32LittleEndian(ReadAtLeast(sizeof(int)))
+        : BinaryPrimitives.ReadInt32BigEndian(ReadAtLeast(sizeof(int)));
 
     public long ReadInt64(Endianness endianness) => endianness == Endianness.Little
-        ? BinaryPrimitives.ReadInt64LittleEndian(ReadBytes(sizeof(long)))
-        : BinaryPrimitives.ReadInt64BigEndian(ReadBytes(sizeof(long)));
+        ? BinaryPrimitives.ReadInt64LittleEndian(ReadAtLeast(sizeof(long)))
+        : BinaryPrimitives.ReadInt64BigEndian(ReadAtLeast(sizeof(long)));
 
     public ushort ReadUInt16(Endianness endianness) => endianness == Endianness.Little
-        ? BinaryPrimitives.ReadUInt16LittleEndian(ReadBytes(sizeof(ushort)))
-        : BinaryPrimitives.ReadUInt16BigEndian(ReadBytes(sizeof(ushort)));
+        ? BinaryPrimitives.ReadUInt16LittleEndian(ReadAtLeast(sizeof(ushort)))
+        : BinaryPrimitives.ReadUInt16BigEndian(ReadAtLeast(sizeof(ushort)));
 
     public uint ReadUInt32(Endianness endianness) => endianness == Endianness.Little
-        ? BinaryPrimitives.ReadUInt32LittleEndian(ReadBytes(sizeof(uint)))
-        : BinaryPrimitives.ReadUInt32BigEndian(ReadBytes(sizeof(uint)));
+        ? BinaryPrimitives.ReadUInt32LittleEndian(ReadAtLeast(sizeof(uint)))
+        : BinaryPrimitives.ReadUInt32BigEndian(ReadAtLeast(sizeof(uint)));
 
     public ulong ReadUInt64(Endianness endianness) => endianness == Endianness.Little
-        ? BinaryPrimitives.ReadUInt64LittleEndian(ReadBytes(sizeof(ulong)))
-        : BinaryPrimitives.ReadUInt64BigEndian(ReadBytes(sizeof(ulong)));
+        ? BinaryPrimitives.ReadUInt64LittleEndian(ReadAtLeast(sizeof(ulong)))
+        : BinaryPrimitives.ReadUInt64BigEndian(ReadAtLeast(sizeof(ulong)));
 }
